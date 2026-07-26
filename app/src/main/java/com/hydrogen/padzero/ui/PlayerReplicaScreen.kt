0d483b86153806a72477f177d349a7a347d9d9b4
LINGrain_L <raincn_yeah@outlook.com>
2026-07-26 12:15:04 +0800

    Update PlayerReplicaScreen.kt

--- app/src/main/java/com/hydrogen/padzero/ui/PlayerReplicaScreen.kt
+++ app/src/main/java/com/hydrogen/padzero/ui/PlayerReplicaScreen.kt
@@ -1,5 +1,8 @@
 package com.hydrogen.padzero.ui
 
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
@@ -17,7 +20,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.verticalScroll
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
